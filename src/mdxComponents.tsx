9a3467d86c4968ff8ddb9b5737b2f9c67c6ca0df
superlucky84 <superlucky84@gmail.com>
2025-01-24 16:07:43 +0900

    middle commit

--- src/mdxComponents.tsx
+++ src/mdxComponents.tsx
@@ -3,9 +3,20 @@ import hljs from 'highlight.js';
 export default {
   code(properties: { className: string }, children: [{ text: string }]) {
     const code = hljs.highlight(children[0].text, {
-      language: properties.className.replace('language-', ''),
+      language: (properties?.className || 'language-javascript').replace(
+        'language-',
+        ''
+      ),
     }).value;
 
     return <code {...properties} innerHTML={code} />;
+  },
+  a({ href, ...properties }: any, children: any) {
+    console.log(href, properties);
+    return (
+      <a href={href} {...properties} target="_blank">
+        {children}
+      </a>
+    );
   },
 };
