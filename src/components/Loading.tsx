b7bfcdcc75f3902b56353c84010fb3dbe721a566
superlucky84 <superlucky84@gmail.com>
2025-02-06 01:11:10 +0900

    feat: loding text verticalAlign

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,10 @@ import { mount } from 'lithent';
 
 const LoadingText = mount(() => {
   return () => (
-    <div class="flex justify-center items-center h-screen text-2xl md:text-4xl font-semibold">
+    <div
+      class="flex justify-center items-center"
+      style={{ height: 'calc(100vh - 120px)' }}
+    >
       <span class="relative before:content-[var(--dots)] animate-dots" />
     </div>
   );
