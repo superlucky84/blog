ab9b11408376549535d7cb93145d5db509de552f
superlucky84 <superlucky84@gmail.com>
2025-02-20 08:09:58 +0900

    fix: logo animation time

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ export default {
       },
       animation: {
         dots: 'dots 1.5s infinite steps(1, end)',
-        glitch: 'glitch 3s ease infinite 3s alternate-reverse',
+        glitch: 'glitch 7s ease infinite 7s alternate-reverse',
       },
       keyframes: {
         dots: {
