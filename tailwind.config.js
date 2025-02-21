38803826c359cba4292d92d9558efd7b601f77df
superlucky84 <superlucky84@gmail.com>
2025-02-22 01:17:59 +0900

    fix: animation duration longterm

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,8 +22,8 @@ export default {
       },
       animation: {
         dots: 'dots 1.5s infinite steps(1, end)',
-        glitch: 'glitch 4000ms',
-        glitchwhite: 'glitchwhite 4000ms',
+        glitch: 'glitch 7000ms',
+        glitchwhite: 'glitchwhite 7000ms',
       },
       keyframes: {
         dots: {
